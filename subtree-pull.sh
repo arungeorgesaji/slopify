e00bf630344bb79c76c0341a63ce9e8b58197a5d
arungeorgesaji <arungeorgesaji@gmail.com>
2026-04-27 14:45:54 +0530

    Removed the ui so I can add it back as a subtree

--- subtree-pull.sh
+++ subtree-pull.sh
@@ -1,3 +1,4 @@
 git subtree pull --prefix=slopify-music slopify-music main --squash
 git subtree pull --prefix=slopify-video slopify-video main --squash
+git subtree pull --prefix=slopify-ui slopify-ui main --squash
 
--- subtree-push.sh
+++ subtree-push.sh
@@ -1,2 +1,3 @@
 git subtree push --prefix=slopify-music slopify-music main
 git subtree push --prefix=slopify-video slopify-video main
+git subtree push --prefix=slopify-ui slopify-ui main
